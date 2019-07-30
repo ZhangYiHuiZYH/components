95648ca6c8c5d0d6f97c5ad4cc51a0a7d01bfbb2
zhangyihui <zhangyihui@100tal.com>
2019-07-30 12:11:54 +0800

    fix element

--- src/components/subject_query.vue
+++ src/components/subject_query.vue
@@ -386,7 +386,7 @@
     // import ElementUI from 'element-ui'
     // import 'element-ui/lib/theme-chalk/index.css'
 
-    Vue.use(ElementUI)
+    // Vue.use(ElementUI)
     export default {
         components: {
             treeRadio:treeRadio
