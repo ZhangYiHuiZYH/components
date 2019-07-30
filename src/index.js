65340877ca0c8375a8eeb15d981df74d217ef20c
zhangyihui <zhangyihui@100tal.com>
2019-07-30 18:37:05 +0800

    提交 clipboard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-chalk/index.css'
 // Vue.use(ElementUI)
-
+import Vue from 'vue'
+import VueClipboard from 'vue-clipboard2'
+Vue.use(VueClipboard)
 
 import msg from './components/subject_query.vue'
 export default msg
